c9a5de6113ddf23051e0bf8ef7f2bfd6926181d7
DoHyeon Lee <cailhuiris@gmail.com>
2020-11-10 14:28:17 +0900

    Work Pool에 힙 인터페이스 구현

--- main.go
+++ main.go
@@ -26,6 +26,36 @@ type Balancer struct {
 	done chan *Work
 }
 
+// Heap 인터페이스 구현 (Duck Typing)
+func (p Pool) Len() int { return len(p) }
+
+func (p Pool) Less(i, j int) bool {
+	return p[i].pending < p[j].pending
+}
+
+func (p *Pool) Swap(i, j int) {
+	a := *p
+	a[i], a[j] = a[j], a[i]
+	a[i].idx = i
+	a[j].idx = j
+}
+
+func (p *Pool) Push(x interface{}) {
+	n := len(*p)
+	item := x.(*Work) // type casting
+	item.idx = n
+	*p = append(*p, item)
+}
+
+func (p *Pool) Pop() interface{} {
+	old := *p
+	n := len(old)
+	item := old[n-1]
+	item.idx = -1
+	*p = old[0:n-1]
+	return item
+}
+
 func (w *Work) doWork(done chan *Work) {
 	for {
 		req := <-w.wok
